9a7bbf9f8a671423f14353cd6e247d69cee89ad2
Matt Varughese <matvarughese@gmail.com>
2017-10-13 22:03:43 +0100

    driver insurance test and functionality

--- app/models/driver_insurance.rb
+++ app/models/driver_insurance.rb
@@ -3,4 +3,9 @@ class DriverInsurance < ActiveRecord::Base
   belongs_to :driver, class_name:  "Partner"
   belongs_to :vehicle
 
+  def number_of_days_to_charge
+    (end_date - start_date).to_i
+  end
+
+
 end
--- spec/models/driver_insurance_spec.rb
+++ spec/models/driver_insurance_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe DriverInsurance, type: :model do
 
+  it "number_of_days_to_charge counts the number of days on cover for a driver_insurance" do
+    driver_insurance = DriverInsurance.create(start_date: Date.today, end_date: Date.today + 1.week)
+    expect(driver_insurance.number_of_days_to_charge).to eq 7
+  end
+
 end
