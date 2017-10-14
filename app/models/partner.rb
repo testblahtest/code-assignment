7846f2f0a55eaac2e99603afc5c31c57efc636df
matvarughese <matvarughese@gmail.com>
2017-10-14 15:01:47 +0100

    Update partner.rb

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,8 +2,8 @@
 
 class Partner < ActiveRecord::Base
 
-  has_many :driver_insurances
-  has_many :owned_vehicles, foreign_key: "owner"
+  has_many :driver_insurances, foreign_key: "driver_id"
+  has_many :owned_vehicles, class_name: "Vehicle", foreign_key: "owner_id"
 
   def total_days_charged_for_all_driver_insurance_policies
 
